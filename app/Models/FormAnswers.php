c23a76bf8272356df5645f4422fffa3ee7142993
JEstebanGnz <juanes01.gm@outlook.com>
2023-06-05 10:27:47 -0500

    dd removed

--- app/Models/FormAnswers.php
+++ app/Models/FormAnswers.php
@@ -89,8 +89,6 @@ class FormAnswers extends Model
     public static function createStudentFormFromRequest(Request $request, Form $form): void
     {
 
-        dd(json_encode($request->input('answers')));
-
         $competencesAverage = self::getCompetencesAverage(json_decode(json_encode($request->input('answers'), JSON_THROW_ON_ERROR), false, 512, JSON_THROW_ON_ERROR));
 
         self::create([
