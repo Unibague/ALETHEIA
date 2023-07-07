c2d5dc9ab633cef7d3cf5fb0105c3330645884a9
JEstebanGnz <juanes01.gm@outlook.com>
2023-07-06 21:07:33 -0500

    sprint reports

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AssessmentPeriod;
+use App\Models\Reports;
+use App\Models\ServiceArea;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -12,5 +16,17 @@ class ReportsController extends Controller
     {
         return Inertia::render('Reports/Index');
     }
+
+
+    public function downloadPDF(Request $request){
+
+        $assessmentPeriodName = AssessmentPeriod::select(['name'])->where('active', '=', 1)->first()->name;
+        $chart = urlencode(json_encode($request->input('myChart')));
+        $pdf = Pdf::loadView('report', compact( 'assessmentPeriodName', 'chart'));
+        return view('report', compact( 'assessmentPeriodName', 'chart'));
+    }
+
+
+    /*      */
 
 }
--- app/Models/Reports.php
+++ app/Models/Reports.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Reports extends Model
 {
     use HasFactory;
+
+
+    public static function downloadPDF($teacherRolesAssessments){
+
+
+
+
+    }
+
+
+
+
+
 }
