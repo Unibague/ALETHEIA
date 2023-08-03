f4308168d890e23f5f9984f530d22953e15559f6
JEstebanGnz <juanes01.gm@outlook.com>
2023-08-03 10:51:11 -0500

    ahora sí final test cronjob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('reports:update')->everyFifteenMinutes();
-/*        $schedule->command('reminder:send')->everyFourMinutes();*/
+        $schedule->command('reminder:send')->everyFourMinutes();
     }
 
     /**
